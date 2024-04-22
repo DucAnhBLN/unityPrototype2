a6a8079a406fab52a9aae4284fce6ebc6986efcd
DucAnhBLN <luongducanh666@gmail.com>
2024-04-22 20:52:38 +0700

    first commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float cooldown = 3;
+    public float lastKeyPressTime = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Time.time - lastKeyPressTime >= cooldown)
+        {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            lastKeyPressTime = Time.time;
+            Debug.Log(lastKeyPressTime);
+        }
+    }
+    }
+}
